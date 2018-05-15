b11883a4fec8e8e850cd5614d85c5a6775d53997
zilani <zilanicse13@gmail.com>
2018-05-15 11:10:02 -0700

    js

---
+++ js/script.js
@@ -0,0 +1,8 @@
+
+
+const button = document.querySelector('.btn')
+const form   = document.querySelector('.form')
+
+button.addEventListener('click', function() {
+   form.classList.add('form--no') 
+});
